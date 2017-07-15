aac069f942cffd1540838346e08d0927a1d4e7b5
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-07-15 12:47:35 +0400

    order service application with scala

---
+++ order-service/src/main/java/orderservice/OrderServiceApplication.java
@@ -0,0 +1,15 @@
+package orderservice;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @author Bhuwan Upadhyay
+ * @date 2017/07/15
+ */
+public class OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+}
