5dd3998ceaa31a53856648e0b6964b3455e84988
Bhuwan Prasad Upadhyay <comp.er.bhuwan@gmail.com>
2017-07-16 09:04:19 +0400

    scala

---
+++ imixs/order-approval-workflow/src/main/java/orderapproval/OrderApprovalWorkflowApplication.java
@@ -0,0 +1,17 @@
+package orderapproval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Bhuwan Upadhyay
+ * @date 2017/07/15
+ */
+@SpringBootApplication
+public class OrderApprovalWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApprovalWorkflowApplication.class, args);
+    }
+
+}
